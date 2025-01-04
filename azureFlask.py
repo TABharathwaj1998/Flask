9153d8c4f4a4fb99bd60010f3301220d5a01bba9
TABharathwaj1998 <62840795+TABharathwaj1998@users.noreply.github.com>
2025-01-04 15:41:41 -0500

    Update azureFlask.py

--- azureFlask.py
+++ azureFlask.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello World! This is Bharathwaj</h1>"
+    return "<h1>Hello World! This is Bharathwaj from ON</h1>"
 
 if __name__=="__main__":
     app.run(debug=True,host="localhost")
