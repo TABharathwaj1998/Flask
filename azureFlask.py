0579e9d964b36d6a60de3418f29b9d899b519776
Bharathwaj <ta.bharathwaj98@gmail.com>
2025-01-19 16:35:08 -0500

    rendered template

--- azureFlask.py
+++ azureFlask.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="template")
 
 @app.route("/")
 def home():
-    output = '''
+    '''output = 
 <h1>Hello World! This is Bharathwaj</h1>
 <h2>Welcome to Azure DevOps</h2>
 <h3>If you want to make changes in web app, update and commit the code on github to trigger the pipelines</h3>
 <h4>Changes made in Github or co-workers can be applied in local using git pull command</h4>
-'''
-    return output
-    #return "<h1>Hello World! This is Bharathwaj</h1>"
+
+    return output'''
+    return render_template("index.html") 
 
 if __name__=="__main__":
     app.run(debug=True,host="localhost")
