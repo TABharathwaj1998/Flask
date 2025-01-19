376f8fe7b13f80d574f2b7365d874952a09906de
TABharathwaj1998 <62840795+TABharathwaj1998@users.noreply.github.com>
2025-01-19 12:36:03 -0500

    Update azureFlask.py

--- azureFlask.py
+++ azureFlask.py
@@ -4,12 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    '''output = '''
-<h1>Hello World! This is Bharathwaj</h1>
-<h2>Welcome to Azure DevOps</h2>
-<h3>If you want to make changes in web app, update and commit the code on github to trigger the pipelines</h3> 
-'''
-    return output'''
     return "<h1>Hello World! This is Bharathwaj</h1>"
 
 if __name__=="__main__":
