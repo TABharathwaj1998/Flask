00f1e0db7d19edfe9a70bb3fc9e38bcfd44e2c40
TABharathwaj1998 <62840795+TABharathwaj1998@users.noreply.github.com>
2025-01-19 13:18:30 -0500

    Update azureFlask.py

--- azureFlask.py
+++ azureFlask.py
@@ -7,7 +7,8 @@ def home():
     output = '''
 <h1>Hello World! This is Bharathwaj</h1>
 <h2>Welcome to Azure DevOps</h2>
-<h3>If you want to make changes in web app, update and commit the code on github to trigger the pipelines</h3> 
+<h3>If you want to make changes in web app, update and commit the code on github to trigger the pipelines</h3>
+<h4>Changes made in Github or co-workers can be applied in local using git pull command</h4>
 '''
     return output
     #return "<h1>Hello World! This is Bharathwaj</h1>"
